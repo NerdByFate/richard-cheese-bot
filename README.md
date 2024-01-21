76928bf009b209422ba49941986667a4b4d2449e
Cherokee Parker <5161492+NerdByFate@users.noreply.github.com>
2024-01-20 18:45:51 -0600

    Make extractors customizable
    *Allow extractor to be specified as an environment variable
    *Exclude YouTube extractors from being valid extractors

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Function
 
-Meet Richard Cheese, a Discord bot built on Node.js. Mr. Cheese will lock on to the player you indicate in environment variable TARGET_USER_ID. When that player has been sitting in a voice channel alone for the defined amount of time, Mr. Cheese will then greet them with one of his cover tracks. Don't ask why -- he just does it.
+Meet Richard Cheese, a Discord bot built on Node.js. Mr. Cheese will lock on to the member you indicate in environment variable TARGET_USER_ID. When that member has been sitting in a voice channel alone for the defined amount of time, Mr. Cheese will then greet them with one of his cover tracks. Don't ask why -- he just does it.
 
 # Build
 
 Environment variables:
-| Name           | Description                                                                               |
-|----------------|-------------------------------------------------------------------------------------------|
-| APP_ID         | The application ID of the Discord application                                             |
-| DISCORD_TOKEN  | The bot token from the Discord application                                                |
-| TARGET_USER_ID | The user ID of the player that Richard should follow (in Discord, right click -> Copy ID) |
-| WAIT_TIME      | The amount of time that Richard should wait before attempting to join the voice channel   |
+| Name           | Description                                                                                                                                                                                                                                                |
+|----------------|------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| APP_ID         | The application ID of the Discord application                                                                                                                                                                                                              |
+| DISCORD_TOKEN  | The bot token from the Discord application                                                                                                                                                                                                                 |
+| TARGET_USER_ID | The user ID of the player that Richard should follow (in Discord, right click -> Copy ID)                                                                                                                                                                  |
+| WAIT_TIME      | The amount of time that Richard should wait before attempting to join the voice channel                                                                                                                                                                    |
+| QUERY_TYPE     | Specifies which service should be used to find tracks (see [discord-player's type.ts enum](https://github.com/Androz2091/discord-player/blob/b677fbf659a2f30535ed57bf0abc9adccc6339ad/packages/discord-player/src/types/types.ts#L207) for valid values)   |
 
-You can specify YouTube queries for tracks to play in the "queries" array of queries.json
+You can specify queries for tracks to play in the "queries" array of queries.json. Queries will be ran using the service you specified in QUERY_TYPE. YouTube is explicitly banned, so if you use a YouTube query type, you will experience errors. Use 'file' as QUERY_TYPE and replace the queries in queries.json with paths to load from files.
--- app.js
+++ app.js
@@ -2,19 +2,30 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const { Player } = require('discord-player');
 const Data = require('./queries.json');
+require('dotenv').config({ path: './.env' });
 
 // Constants
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates] });
 const player = new Player(client);
+initializeExtractors();
 
 // Functions
+
+/**
+ * @summary Loads the default extractors (except for YouTube).
+ * @async
+ * @remarks Discord bots are not allowed to stream from YouTube, so we must manually exclude it here.
+ */
+async function initializeExtractors() {
+   await player.extractors.loadDefault((ext) => ext !== 'YouTubeExtractor');
+}
 
 /**
  * @summary Plays a random song from the queries specified in queries.json.
  * This function will attempt to play again if it is unable to play due to an exception (e.g. the song cannot be found on YouTube).
  * This will increment errors until errors is greater than 10, after which it will stop attempting to play.
  * @async
- * @param {VoiceState} state - A VoiceState object representing the current voice state of a Guild Member
+ * @param {VoiceState} state - A VoiceState object representing the voice state of a Guild Member
  * @param {number} errors - The number of errors that have been encountered while attempting to play (optional)
  * @returns {GuildQueue} A queue containing the track and state metadata associated with the song chosen from queries.json
  */
@@ -22,7 +33,7 @@ async function play(state, errors) {
   let rand = Math.floor(Math.random() * Data.queries.length);
   
   try {
-    let dataOut = await player.play(state.channel, Data.queries[rand], { searchEngine: 'youtube', nodeOptions: { leaveOnEmpty: true, metadata: state }, connectionOptions: { deaf: false } });
+    let dataOut = await player.play(state.channel, Data.queries[rand], { searchEngine: process.env.QUERY_TYPE, nodeOptions: { leaveOnEmpty: true, metadata: state }, connectionOptions: { deaf: false } });
     return dataOut.queue;
   }
   catch (error) {
@@ -43,7 +54,7 @@ async function play(state, errors) {
  * environment variable WAIT_TIME. Playback does not begin if the right GuildMembers are not present in
  * the channel after the wait time has elapsed.
  * @async
- * @param {VoiceState} state - A VoiceState object representing the current voice state of a Guild Member
+ * @param {VoiceState} state - A VoiceState object representing the voice state of a Guild Member
  * @param {VoiceChannel} channel - The VoiceChannel of the channel to play the song in
  */
 async function playAfterWaitTime(state, channel) {
@@ -61,35 +72,35 @@ async function playAfterWaitTime(state, channel) {
  * @summary Plays a random song if the target member is in position.
  * The function first decides whether that member is the target member and is alone in a voice channel.
  * If so, plays a random song from queries.json in that voice channel.
- * @param {VoiceState} oldState - A VoiceState object representing the previous voice state of a Guild Member
- * @param {VoiceState} newState - A VoiceState object representing the current voice state of a Guild Member
+ * @param {VoiceState} state - A VoiceState object representing the voice state of a Guild Member
  */
-function voiceStateUpdateCallback(oldState, newState) {
-  console.log(`${newState.channelId}: User ${newState.member.user.username} joined the channel.`);
-  if (newState.channelId !== null && rightMembersArePresent(newState.channel.members)) {
-    if (newState.member.id === process.env.TARGET_USER_ID) {
-      console.log(`${newState.member.user.username} is in position, executing protocol D.CH3353.`);
+function playIfVoiceStateValid(state) {
+  if (state.channelId !== null && rightMembersArePresent(state.channel.members)) {
+    if (state.channel.members.size < 2) {
+      console.log(`${state.channel.members.get(process.env.TARGET_USER_ID).user.username} is in position, executing protocol D.CH3353.`);
       
-      playAfterWaitTime(newState, client.channels.cache.get(newState.channelId))
+      playAfterWaitTime(state, client.channels.cache.get(state.channelId));
     }
     else {
-      console.log('Protocol D.CH3353 is now in action.')
+      console.log('Protocol D.CH3353 is now in action.');
     }
   }
   else {
-    stopPlayback(newState);
+    stopPlayback(state);
   }
 }
 
 /**
- * @summary Stops all playback from this bot in a particular Guild.
- * The function does this by first finding the queue associated with the channel of that Guild.
- * Then, it deletes it if it has not already been deleted.
- * @param {VoiceState} state - A VoiceState object representing the current voice state of a Guild Member
+ * @summary Stops all playback from this bot in a particular channel.
+ * The function does this by first finding the queue associated with the Guild of the VoiceState.
+ * Then, it deletes it if the channel matches the VoiceState and it has not already been deleted.
+ * @param {VoiceState} state - A VoiceState object representing the voice state of a Guild Member
  */
 function stopPlayback(state) {
   const queue = player.nodes.get(state.guild.id);
-  if (queue && !queue.deleted) queue.delete();
+  if (queue && !queue.deleted && queue.channel && queue.channel.id === state.channelId) {
+    queue.delete();
+  }
 }
 
 /**
@@ -101,14 +112,14 @@ function stopPlayback(state) {
  */
 function rightMembersArePresent(members) {
   let targetPresent = 0;
-  let richardPresent = 0;
+  let botPresent = 0;
   
   members.forEach(member => {
     targetPresent = (member.id === process.env.TARGET_USER_ID) ? (1) : (targetPresent);
-    richardPresent = (member.id === process.env.APP_ID) ? (1) : (richardPresent);
+    botPresent = (member.id === process.env.APP_ID) ? (1) : (botPresent);
   });
   
-  return (targetPresent === 1 && targetPresent + richardPresent === members.size) ? (true) : (false);
+  return (targetPresent === 1 && targetPresent + botPresent === members.size) ? (true) : (false);
 }
 
 // Events
@@ -116,7 +127,14 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("voiceStateUpdate", voiceStateUpdateCallback);
+client.on("voiceStateUpdate", (oldState, newState) => {
+  console.log(`${newState.channelId}: User ${newState.member.user.username} joined the channel.`);
+  playIfVoiceStateValid(newState);
+  
+  if (oldState.channelId !== newState.channelId) {
+    playIfVoiceStateValid(oldState);
+  }
+});
 
 player.events.on('emptyQueue', (queue) => {
   playAfterWaitTime(queue.metadata, client.channels.cache.get(queue.metadata.channelId));
